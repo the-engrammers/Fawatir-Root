37ba4293f05fce4b92173e602aadc98fa814bbf4
Xbalawi <maazouzi7omar@gmail.com>
2026-08-10 18:02:46 +0000

    commit

--- fawatir_frontend/next.config.mjs
+++ fawatir_frontend/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone", // Required for your Docker setup to work
+  // output: "standalone", // Required for your Docker setup to work
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
